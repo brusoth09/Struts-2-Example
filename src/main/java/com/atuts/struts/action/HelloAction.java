b73d7e85b16df5b4c526422189cc5ca51caed723
brusoth09 <brusoth09@email.com>
2016-05-26 16:07:30 +0530

    struts 2 interceptors example

--- src/main/java/com/atuts/struts/action/HelloAction.java
+++ src/main/java/com/atuts/struts/action/HelloAction.java
@@ -1,7 +1,15 @@
 package com.atuts.struts.action;
 
+import com.opensymphony.xwork2.ActionSupport;
+
 /**
  * Created by brusoth on 5/26/16.
  */
-public class HelloAction {
+public class HelloAction extends ActionSupport {
+    public String execute() throws Exception {
+
+        System.out.println("HelloAction execute() is called");
+        return SUCCESS;
+
+    }
 }
--- src/main/java/com/atuts/struts/interceptors/PrintMsgInterceptors.java
+++ src/main/java/com/atuts/struts/interceptors/PrintMsgInterceptors.java
@@ -1,7 +1,30 @@
 package com.atuts.struts.interceptors;
 
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
 /**
  * Created by brusoth on 5/26/16.
  */
-public class PrintMsgInterceptors {
+public class PrintMsgInterceptors implements Interceptor {
+    @Override
+    public void destroy() {
+        System.out.println("CustomInterceptor destroy() is called...");
+    }
+
+    @Override
+    public void init() {
+        System.out.println("CustomInterceptor init() is called...");
+    }
+
+    @Override
+    public String intercept(ActionInvocation actionInvocation) throws Exception {
+        System.out.println("CustomInterceptor, before invocation.invoke()...");
+
+        String result = actionInvocation.invoke();
+
+        System.out.println("CustomInterceptor, after invocation.invoke()...");
+
+        return result;
+    }
 }
